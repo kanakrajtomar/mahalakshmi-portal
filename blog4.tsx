00b1bd53e6d8c55c69a4f821b685a754adf553dd
Kanak Raj Tomar <112727321+kanakrajtomar@users.noreply.github.com>
2024-01-05 12:49:09 +0530

    Create blog4.tsx

---
+++ blog4.tsx
@@ -0,0 +1,364 @@
+import { FunctionComponent } from "react";
+import styles from "./Blog4.module.css";
+
+const Blog4: FunctionComponent = () => {
+  return (
+    <div className={styles.blog4}>
+      <img
+        className={styles.backgroundPatternsIcon}
+        alt=""
+        src="/background-patterns@2x.png"
+      />
+      <div className={styles.title}>
+        <div className={styles.newsArticles}>{`News & Articles`}</div>
+        <div
+          className={styles.stayUpdatedWith}
+        >{`Stay updated with the latest news in healthcare `}</div>
+      </div>
+      <div className={styles.navBar}>
+        <div className={styles.navBarChild} />
+        <div className={styles.frameParent}>
+          <div className={styles.groupParent}>
+            <img
+              className={styles.frameChild}
+              alt=""
+              src="/group-2216@2x.png"
+            />
+            <b className={styles.mahalakshmiAgencies}>Mahalakshmi Agencies</b>
+          </div>
+          <div className={styles.homeParent}>
+            <div className={styles.home}>Home</div>
+            <div className={styles.home}>About us</div>
+            <div className={styles.home}>Product corner</div>
+            <div className={styles.home}>Contact us</div>
+            <div className={styles.home}>{`News & More`}</div>
+          </div>
+          <div className={styles.button}>
+            <div className={styles.buttonChild} />
+            <div className={styles.letsConnect}>Let’s connect</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.footerChild} />
+        <div className={styles.deviconPlainlinkedinParent}>
+          <img
+            className={styles.deviconPlainlinkedin}
+            alt=""
+            src="/deviconplainlinkedin@2x.png"
+          />
+          <img
+            className={styles.icbaselineQuoraIcon}
+            alt=""
+            src="/icbaselinequora@2x.png"
+          />
+          <img
+            className={styles.deviconPlainlinkedin}
+            alt=""
+            src="/mdiinstagram@2x.png"
+          />
+        </div>
+        <b className={styles.mahalakshmiAgencies1991Container}>
+          <span>/</span>
+          <span className={styles.mahalakshmiAgencies1991}>
+            Mahalakshmi Agencies 1991
+          </span>
+        </b>
+        <div className={styles.companyParent}>
+          <div className={styles.company}>Company</div>
+          <div className={styles.aboutUsParent}>
+            <div className={styles.mahalakshmiAgencies}>About Us</div>
+            <div className={styles.mahalakshmiAgencies}>News</div>
+            <div className={styles.mahalakshmiAgencies}>Product Corner</div>
+            <div className={styles.mahalakshmiAgencies}>Contact us</div>
+          </div>
+        </div>
+        <div
+          className={styles.copyright2023All}
+        >{`@ Copyright 2023, All Rights Reserved. `}</div>
+        <div className={styles.footerInner}>
+          <div className={styles.privacyPolicyParent}>
+            <div className={styles.mahalakshmiAgencies}>Privacy Policy</div>
+            <div className={styles.mahalakshmiAgencies}>Contact us</div>
+            <div
+              className={styles.mahalakshmiAgencies}
+            >{`Terms & Conditions`}</div>
+          </div>
+        </div>
+        <div className={styles.resourcesParent}>
+          <div className={styles.company}>Resources</div>
+          <div className={styles.aboutUsParent}>
+            <div className={styles.mahalakshmiAgencies}>Latest News</div>
+            <div className={styles.mahalakshmiAgencies}>Quora</div>
+            <div className={styles.mahalakshmiAgencies}>LinkedIn</div>
+          </div>
+        </div>
+        <div className={styles.emailUsParent}>
+          <div className={styles.emailUs}>EMAIL US</div>
+          <div className={styles.adminmahalakshmiagesncies1991}>
+            info@mahalakshmiagencies1991.com
+          </div>
+        </div>
+        <div className={styles.callUsParent}>
+          <div className={styles.callUs}>Call US</div>
+          <div className={styles.div}>080-42035312</div>
+          <div className={styles.div}>+91 6360166226</div>
+        </div>
+        <div className={styles.subscribeToBeUpdatedWithLParent}>
+          <div className={styles.company}>
+            Subscribe to be updated with lates news
+          </div>
+          <div className={styles.frameGroup}>
+            <div className={styles.enterEmailAddressWrapper}>
+              <div className={styles.mahalakshmiAgencies}>
+                Enter Email Address
+              </div>
+            </div>
+            <div className={styles.joinWrapper}>
+              <b className={styles.mahalakshmiAgencies}>Join</b>
+            </div>
+          </div>
+        </div>
+        <div className={styles.footerItem} />
+      </div>
+      <div className={styles.content}>
+        <div className={styles.div2}>
+          <div className={styles.content1}>
+            <div className={styles.image}>
+              <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+              <div className={styles.tag}>
+                <div className={styles.tag1}>
+                  <b className={styles.operation}>Operation</b>
+                </div>
+                <div className={styles.tag2}>
+                  <b className={styles.operation}>Surgical</b>
+                </div>
+              </div>
+            </div>
+            <div className={styles.content2}>
+              <div className={styles.subTitle}>
+                <div className={styles.november152023}>November 15, 2023</div>
+                <div className={styles.mahalakshmiAgenciesA}>
+                  Mahalakshmi Agencies: A Holistic Approach to Combat Antibiotic
+                  Resistance with Diverse Healthcare Solutions
+                </div>
+              </div>
+              <div className={styles.subContent}>
+                <div className={styles.inTheRelentless}>
+                  In the relentless struggle against antibiotic resistance,
+                  Mahalakshmi Agencies stands as a beacon of innovation and
+                  commitment, offering a comprehensive portfolio of healthcare
+                  solutions aimed at minimising infections.
+                </div>
+                <div className={styles.button1}>
+                  <b className={styles.readMore}>Read More</b>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.div3}>
+          <div className={styles.content1}>
+            <div className={styles.image}>
+              <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+              <div className={styles.tag}>
+                <div className={styles.tag1}>
+                  <b className={styles.operation}>Analysis</b>
+                </div>
+                <div className={styles.tag2}>
+                  <b className={styles.operation}>Bbraun</b>
+                </div>
+              </div>
+            </div>
+            <div className={styles.content2}>
+              <div className={styles.subTitle}>
+                <div className={styles.november152023}>December 02, 2023</div>
+                <div className={styles.mahalakshmiAgenciesA}>
+                  Innovating Patient Care: Unveiling B Braun's Cutting-Edge
+                  Infusion Solutions
+                </div>
+              </div>
+              <div className={styles.subContent1}>
+                <div className={styles.inTheDynamic}>
+                  In the dynamic realm of healthcare, infusion therapy plays a
+                  pivotal role in patient care, and no name resonates more
+                  profoundly than B Braun.
+                </div>
+                <div className={styles.button2}>
+                  <b className={styles.readMore}>Read More</b>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.sports}>
+        <div className={styles.sports1}>
+          <div className={styles.latestNews1}>Latest News</div>
+          <div className={styles.button3}>
+            <img
+              className={styles.rightButtonIcon}
+              alt=""
+              src="/right-button@2x.png"
+            />
+            <img
+              className={styles.leftButtonIcon}
+              alt=""
+              src="/left-button@2x.png"
+            />
+          </div>
+        </div>
+        <img className={styles.lineIcon} alt="" src="/line@2x.png" />
+        <div className={styles.post1}>
+          <img className={styles.imageIcon2} alt="" src="/image@2x.png" />
+          <div className={styles.dec2023}>
+            <span>{` `}</span>
+            <span className={styles.dec20231}>23 Dec 2023</span>
+          </div>
+          <div className={styles.leadingChangeIn}>
+            {" "}
+            Leading Change in Healthcare: New Products and Protocols
+          </div>
+          <div className={styles.mahalakshmiAgenciesRecently}>
+            Mahalakshmi Agencies recently introduced advanced healthcare
+            technology at Bhagwan Mahaveer Jain Hospital, providing hands-on
+            training for Healthcare Professionals. This collaboration emphasizes
+            integrating cutting-edge solutions into treatment and care. Through
+            direct interaction, Mahalakshmi Agencies reaffirmed its commitment
+            to enhancing patient care and operational efficiency. This
+            initiative underscores the synergy between Mahalakshmi Agencies and
+            Bhagwan Mahaveer Jain Hospital, demonstrating their shared
+            dedication to innovative healthcare solutions.
+          </div>
+          <div className={styles.cricket}>
+            <div className={styles.cricketChild} />
+            <div className={styles.healthcare}>Healthcare</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.post11}>
+        <img className={styles.imageIcon2} alt="" src="/image@2x.png" />
+        <div className={styles.dec2023}>
+          <span>{` `}</span>
+          <span className={styles.dec20231}>27 Dec 2023</span>
+        </div>
+        <div className={styles.leadingChangeIn}>
+          Clinical Evolution: Mastering New Processes in Medicine
+        </div>
+        <div className={styles.introducingNewProducts}>
+          Introducing new products and protocols in healthcare is pivotal for
+          enhancing patient care and outcomes. This involves merging innovative
+          medical technologies with practical applications, focusing on
+          improving diagnosis, treatment, and patient management. It's a
+          collaborative effort requiring alignment among healthcare
+          professionals, researchers, and industry experts to ensure safety,
+          efficacy, and seamless integration into existing healthcare systems.
+        </div>
+        <div className={styles.cricket}>
+          <div className={styles.cricketItem} />
+          <div className={styles.healthcare}>Healthcare</div>
+        </div>
+      </div>
+      <div className={styles.contentWrapper}>
+        <div className={styles.content5}>
+          <div className={styles.div2}>
+            <div className={styles.content1}>
+              <div className={styles.image}>
+                <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+                <div className={styles.tag}>
+                  <div className={styles.tag1}>
+                    <b className={styles.operation}>Operation</b>
+                  </div>
+                  <div className={styles.tag2}>
+                    <b className={styles.operation}>Surgical</b>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.content2}>
+                <div className={styles.subTitle}>
+                  <div className={styles.november152023}>November 15, 2023</div>
+                  <div className={styles.mahalakshmiAgenciesA}>
+                    The silent threat: Antimicrobial Resistance
+                  </div>
+                </div>
+                <div className={styles.subContent}>
+                  <div className={styles.inTheRelentless}>
+                    In the relentless struggle against antibiotic resistance,
+                    Mahalakshmi Agencies stands as a beacon of innovation and
+                    commitment, offering a comprehensive portfolio of healthcare
+                    solutions aimed at minimising infections.
+                  </div>
+                  <div className={styles.button1}>
+                    <b className={styles.readMore}>Read More</b>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.div3}>
+            <div className={styles.content1}>
+              <div className={styles.image}>
+                <img className={styles.imageIcon} alt="" src="/image@2x.png" />
+                <div className={styles.tag}>
+                  <div className={styles.tag1}>
+                    <b className={styles.operation}>Analysis</b>
+                  </div>
+                  <div className={styles.tag2}>
+                    <b className={styles.operation}>Bbraun</b>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.content2}>
+                <div className={styles.subTitle}>
+                  <div className={styles.november152023}>December 02, 2023</div>
+                  <div className={styles.mahalakshmiAgenciesA}>
+                    Spread of Antimicrobial Resistance
+                  </div>
+                </div>
+                <div className={styles.subContent1}>
+                  <div className={styles.inTheDynamic}>
+                    In the dynamic realm of healthcare, infusion therapy plays a
+                    pivotal role in patient care, and no name resonates more
+                    profoundly than B Braun.
+                  </div>
+                  <div className={styles.button2}>
+                    <b className={styles.readMore}>Read More</b>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.newslatter}>
+        <div className={styles.backgroundParent}>
+          <img
+            className={styles.backgroundIcon}
+            alt=""
+            src="/background@2x.png"
+          />
+          <div className={styles.content10}>
+            <div className={styles.content11}>
+              <div className={styles.text}>
+                <div className={styles.neverMissAny}>
+                  Never miss any update, subscribe to our news letter.
+                </div>
+              </div>
+              <div className={styles.button6}>
+                <div className={styles.button7}>
+                  <b className={styles.yourMailHere}>your mail here...</b>
+                </div>
+                <div className={styles.button8}>
+                  <b className={styles.yourMailHere}>Join Now</b>
+                </div>
+              </div>
+            </div>
+          </div>
+          <img className={styles.pills1Icon} alt="" src="/pills-1@2x.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog4;
